36e84b7c756cf33ddf671dd3c9921bf93ddb5275
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 14:47:52 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-/*
+/**
+ * puts - "Programming is like building a multilingual puzzle
  *
- * main -prints the size of various types
- *
- * Return: 0 if exited properly, non-zero otherwise
- *
+ * Return: 0
  */
-int main(void)
+int puts(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+	puts(""Programming is like building a multilingual puzzle"\n);
+	Return (0);
+}
