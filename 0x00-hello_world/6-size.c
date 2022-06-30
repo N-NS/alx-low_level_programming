2caf4f29e7eeb128f8fcaba7ae4e342bdbd9bcc1
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 15:47:50 -0700

    s

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf(Size of a long long int: 8 byte(s)");
+	return (0);
+}
