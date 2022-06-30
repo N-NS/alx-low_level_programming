4e4ed3826cd8a3cbefca4cfd75ea40d3553918ac
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 14:55:47 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
- * main - "Programming is like building a multilingual puzzle
+ * main - prints the size of various types
  *
- * Return: 0
+ * Return: 0 if exited properly, non-zero otherwise
+ *
  */
 int main(void)
 {
