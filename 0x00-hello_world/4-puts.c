1e988079dec58430d366768617638a83626aa97a
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 15:31:57 -0700

    v

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* main -print using puts command*/
+/*
+ *  main -print using puts command
+ *
+ *  return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
