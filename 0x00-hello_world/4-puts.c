ba7a9900c8e6f062695b20db1826cfada0a421ed
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 14:32:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
-
-/**
+#include<stdio.h>
+/*
  *
- *  * main - Entry point
+ * main -prints the size of various types
  *
- *   * 
+ * Return: 0 if exited properly, non-zero otherwise
  *
- *    * Description: 'the program's description'
- *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 
