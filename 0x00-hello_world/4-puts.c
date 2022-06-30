90c89502a691c6168379e002d44390da75344c5a
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-06-30 10:32:33 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Decription:use puts to print text
- *
- * Return: 0
- */
-
 int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle");
