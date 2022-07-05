f007fd59a6fc867eadd08aeb1d560e6021f71724
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-07-05 11:51:22 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -print alphabet in lowercase
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar (10); /* ascii code for return*/
+
+	return (0);
+}
