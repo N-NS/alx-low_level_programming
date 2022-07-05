6b5fefd690ef1fe921cbfb45b962344508b1ade0
N-NS <nyamburanjani@students.uonbi.ac.ke>
2022-07-05 12:28:21 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet -print alphabet in lowercase
+ * print_alphabet -prints the alphabet in lowercase
  */
 void print_alphabet(void)
 {
@@ -11,5 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar (ch);
 	}
-	_putchar ('\n')
+	_putchar ('\n');
 }
